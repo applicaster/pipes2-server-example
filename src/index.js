e89be55894f16656627e1c36c2e45e964fd03ca1
gavriguy <gavri.guy@gmail.com>
2021-01-28 19:11:10 +0200

    setup Heroku

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const routes = require("./routes");
 const path = require("path");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const options = {
   definition: {
