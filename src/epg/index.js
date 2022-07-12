4977258fef847246a3cf4389e753b76c8581b385
Francois roland <f.roland@applicaster.com>
2022-07-12 11:58:05 +0200

    feat: add randomized program endpoint

--- src/epg/index.js
+++ src/epg/index.js
@@ -9,10 +9,29 @@ const {
 } = require("./channels");
 
 const { programFeed, createProgramData } = require("./programs");
-const { clearCache } = require("./programCache");
+const { clearCache, getAllCache } = require("./programCache");
 
 module.exports.setup = (app) => {
-  app.get("/epg_channels", (req, res) => {
+  /**
+   * @swagger
+   * /epg/channels:
+   *  get:
+   *    description: Get all channels
+   *
+   *    parameters:
+   *     - in: query
+   *       name: count
+   *       description: Number of channels to return
+   *       schema:
+   *        type: "number"
+   *       required: false
+   *       default: 10
+   *
+   *    responses:
+   *      200:
+   *       description: A feed of channels
+   */
+  app.get("/epg/channels", (req, res) => {
     res.setHeader("content-type", "application/vnd+applicaster.pipes2+json");
     res.setHeader("Cache-Control", "public, max-age=300");
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -34,11 +53,95 @@ module.exports.setup = (app) => {
     res.json(feed);
   });
 
-  app.get("/epg_programs", (req, res) => {
+  /**
+   * @swagger
+   * /epg/programs:
+   *  get:
+   *    description: Get Programs for a given channel
+   *
+   *    parameters:
+   *     - in: query
+   *       name: channel
+   *       description: channel id for the program
+   *       schema:
+   *        type: "string"
+   *       required: true
+   *
+   *     - in: query
+   *       name: timezone
+   *       description: timezone of the client
+   *       schema:
+   *        type: timezone offset (utc(+|-)x)
+   *       required: false
+   *       default: "utc"
+   *
+   *     - in: query
+   *       name: start_time
+   *       description: start time of the programs
+   *       schema:
+   *        type: ISO date string
+   *       required: false
+   *       default: "now"
+   *
+   *    responses:
+   *      200:
+   *        description: A list of programs
+   */
+  app.get("/epg/programs", async (req, res) => {
     res.setHeader("content-type", "application/vnd+applicaster.pipes2+json");
     res.setHeader("Cache-Control", "public, max-age=300");
     res.setHeader("Access-Control-Allow-Origin", "*");
 
-    const { channel } = req.query;
+    const { channel: channelId, timezone = "utc", start_time } = req.query;
+
+    const startTime = start_time
+      ? DateTime.fromISO(start_time)
+      : DateTime.now();
+
+    if (!channelId) {
+      res.status(402).send("Missing channel id");
+    }
+
+    const channel = R.find(R.propEq("id", channelId), channelData);
+
+    const programs = await createProgramData({
+      timezone,
+      startTime,
+      channelId
+    });
+
+    const feed = programFeed(programs, {
+      channel: channel?.title || "unknown"
+    });
+
+    res.json(feed);
+  });
+
+  /**
+   * @swagger
+   * /epg/clear_cache:
+   *  get:
+   *    description: Clears the server random programs cache
+   *  responses:
+   *    200:
+   *     description: { success: true }
+   */
+  app.get("/epg/clear_cache", (req, res) => {
+    clearCache();
+    res.json({ success: true });
+  });
+
+  /**
+   * @swagger
+   * /epg/cache:
+   *  get:
+   *    description: returns the random program cache
+   *  responses:
+   *    200:
+   *     description: A list of programs cached per date
+   */
+  app.get("/epg/cache", (req, res) => {
+    const cache = getAllCache();
+    res.json(cache);
   });
 };
